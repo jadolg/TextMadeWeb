1c4596c8af3a9a3b583058d6fdf8146593f2b133
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-03-30 11:24:54 -0400

    no proxy on production

--- textify/views.py
+++ textify/views.py
@@ -71,8 +71,8 @@ def insert_cleaner_url(data, url):
 def textify_it(request, url):
     if url.startswith('http://') or url.startswith('https://'):
         try:
-            page = requests.get(url, verify=False, proxies=proxyDict)
-            # page = requests.get(url, verify=False)
+            # page = requests.get(url, verify=False, proxies=proxyDict)
+            page = requests.get(url, verify=False)
         except:
             return render(request, 'search.html', {'message': 'There was an error trying to open ' + url})
         if page.status_code == 200:
